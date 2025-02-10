2060ece8700bc8284f2a75ec4b627d1192cc7129
GPS\vitec <vugar.elyasov@bk.ru>
2025-02-10 18:32:27 +0400

    Create file test.js and tested 10 tasks

---
+++ test.js
@@ -0,0 +1,207 @@
+ 
+
+// let arr = [1,5,6,7]
+
+
+
+// const sumArray = arr.reduce((sumArr,element)=>{
+//    return sumArr + element
+// },0)
+
+// console.log(sumArray)
+// const newarr = arr.map((i) => i+2)
+
+// console.log(newarr)
+
+// newarr.push(98)
+
+// console.log(newarr)
+
+// console.log(arr)
+
+// newarr.pop()
+
+// console.log(newarr)
+
+
+// newarr.unshift(52)
+
+// console.log(newarr)
+
+// newarr.shift(52)
+
+// console.log(newarr)
+
+
+// let obj = {
+//     car: "Hyundai",
+//     price: 10 ,
+//     contry: "Japan"
+// }
+
+// const obj1 = JSON.parse(JSON.stringify(obj))
+// console.log(obj1)
+
+// const {price,...car_country} = obj
+// console.log(car_country)
+
+// const {price : newprice,...car_country} = obj
+// const newobj = {newprice,...car_country}
+// console.log(newobj)
+
+
+
+// const arr2 = [{car: "Toyota", model: "Camry"},{car: "Opel",model: "Astra"},{car: "Mitsubishi",model: "Pajero"}]
+
+// const arr3 = arr2.find((object)=>{
+//    return object.model === "Camry"
+// })
+ 
+// console.log(arr3)
+
+// const arr2 = {car: "Opel",model: "Astra",country: "Italy"}
+
+// const arrKeys = Object.keys(arr2)
+
+// const arrValues = Object.values(arr2)
+
+
+// console.log(arrKeys)
+
+// console.log(arrValues)
+
+// let objectFromArray = arrKeys.reduce((array,key,index)=>{
+//       array[key] = arrValues[index]
+//       return array  
+// }, {})
+// console.log(objectFromArray)
+
+
+    // Задача 1 
+
+// const myCountry = "Azerbaijan"
+
+// console.log(myCountry)
+
+    //Задача 2 
+
+//  let myCountry
+ 
+//  myCountry = true
+
+//  console.log(myCountry)
+
+//  myCountry = false
+
+//  console.log(myCountry)
+
+    //Задача 3 //   
+
+    // let a = 9s
+ 
+    // let b = 10
+
+    // let c = a*b
+    // console.log(c)
+
+    // Задача 4
+    
+    // let name = "Vuqar"
+    
+    // let surname = "Elyasov"
+
+    // let profession = "Frontend"
+
+
+    // let allText1 = 'Меня зовут' + ' ' + name +' '+surname + ' ' + 'и я' + ' ' + profession + ' ' + 'разработчик'
+    
+    // let allText2 = `Меня зовут ${name} ${surname} и я ${profession} разработчик`
+
+    // console.log(allText1)
+    // console.log(allText2)
+
+    // Задача 5
+
+    /**Комеентарии
+     * первый комментарий
+     * второй
+     * третий
+     */
+
+    // Задача 6
+
+    // let age = 6 , str = 'Car' , bool = true , unDefined = null 
+
+    // let arr = [15,'salam',0]
+
+    // let obj = {
+    //     car: 'Hyudai',
+    //     model: 'Elantra',
+    //     price: undefined
+    // }
+
+    // let a
+
+    // console.log(typeof(a))
+    // console.log(typeof(age))
+    // console.log(typeof(str))
+    // console.log(typeof(bool))
+    // console.log(typeof(unDefined))
+    // console.log(typeof(arr))
+    // console.log(typeof obj)
+
+    // Задача 7 
+
+    // function myName (){
+    //    let a = 'Vuqar'
+
+    //    console.log(a)
+
+    //    return a
+    // }
+
+    // const a = myName()
+    // console.log(a)
+    
+    // Задача 8
+
+    // let testFunction = function (name){
+    
+    //     return `Привет ${name}`
+    // }
+
+    // const functionExample = testFunction('Vuqar')
+  
+
+    // console.log(functionExample);
+
+    // Задача 9
+
+    // Variant 1 s
+
+    // let testFunction2 = () => 'Привет Мир'
+    
+    // console.log(testFunction2())
+
+    // Variant 2
+
+    // let testFunction3 = () => {
+    //     return 'Привет Мир'
+    // }
+    // console.log(testFunction3())
+
+    // Задача 10
+    
+    // /**
+    //  * Вычисляем разницу двух чисел
+    //  * @param {number} a 
+    //  * @param {number} b 
+    //  * @returns {number} Разница чисел
+    //  */
+    // const difference = (a,b) => {
+    //     return a-b
+    // }
+
+    // console.log(difference(10,9))
+
+    
